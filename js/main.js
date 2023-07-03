75de0fb19b8cb312e2c739886b9ca3c8cc1fa614
leoroca98 <leoroca12345@gmail.com>
2023-07-03 17:12:41 +0200

    calcolo biglietto per minorenni

--- js/main.js
+++ js/main.js
@@ -7,15 +7,38 @@ console.log(etaPasseggero);
 //Calcolare il prezzo del biglietto 
 
 function calcoloPrezzoBiglietto(distanza) {
-    const prezzoBasePerKm = 0.20;
-    const prezzoTotale = distanza * prezzoBasePerKm; 
+    let prezzoBasePerKm = 0.20;
+    let prezzoTotale = distanza * prezzoBasePerKm; 
 
     return prezzoTotale;
 }
 
-const distanzaViaggio = 142;
+let distanzaViaggio = 142;
 
-const prezzoBiglietto = calcoloPrezzoBiglietto(distanzaViaggio);
+let prezzoBiglietto = calcoloPrezzoBiglietto(distanzaViaggio);
 console.log("Prezzo del biglietto:", prezzoBiglietto.toFixed(2), "euro");
+
+// sconto del 20% per i minorenni 
+
+function calcoloPrezzoBiglietto(distanza, perMinorenni) {
+    let prezzoBasePerKm = 0.20;
+    let prezzoTotale = distanza * prezzoBasePerKm;
+
+    if (perMinorenni) {
+        let scontoPercentuale =0.20;
+        let sconto = prezzoBiglietto * scontoPercentuale;
+        prezzoTotale -=sconto;
+    }
+    return prezzoTotale;
+}
+
+let distanzaViaggioMinorenni = 142;
+let perMinorenni = true;
+ 
+let prezzoBigliettoMinorenni = calcoloPrezzoBiglietto(distanzaViaggio, perMinorenni);
+console.log("prezzo del biglietto:" , prezzoBigliettoMinorenni.toFixed(2), "euro");
+
+
+
 
 
