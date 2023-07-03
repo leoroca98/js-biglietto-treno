0472e2cf113089e2e4f717c9d86c9c32f714dc2e
leoroca98 <leoroca12345@gmail.com>
2023-07-03 16:49:27 +0200

    prima parte conclusa

--- js/main.js
+++ js/main.js
@@ -6,4 +6,16 @@ console.log(etaPasseggero);
 
 //Calcolare il prezzo del biglietto 
 
+function calcoloPrezzoBiglietto(distanza) {
+    const prezzoBasePerKm = 0.20;
+    const prezzoTotale = distanza * prezzoBasePerKm; 
+
+    return prezzoTotale;
+}
+
+const distanzaViaggio = 142;
+
+const prezzoBiglietto = calcoloPrezzoBiglietto(distanzaViaggio);
+console.log("Prezzo del biglietto:", prezzoBiglietto.toFixed(2), "euro");
+
 
