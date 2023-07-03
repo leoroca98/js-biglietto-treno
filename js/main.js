5a8fbf33f46555c09783681b5b7de45b3cc23642
leoroca98 <leoroca12345@gmail.com>
2023-07-03 16:35:18 +0200

    inzio esercizio

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let kilometriPercosrsi = prompt("quanti kilometri vorresti percorrere?");
+let etaPasseggero = prompt("quanti anni hai?");
+
+console.log(kilometriPercosrsi);
+console.log(etaPasseggero);
+
+//Calcolare il prezzo del biglietto 
+
+
